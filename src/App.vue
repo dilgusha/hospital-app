1f1bf99da38e17ecfaa1ebf6f485d9a2a9fa4a8a
dilgusha <dila.hesenli5@gmail.com>
2026-02-19 10:11:26 +0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
-import { RouterView } from 'vue-router';
-import Footer from '@/components/layout/Footer.vue';
-import Header from '@/components/layout/Header.vue';
-import ScrollToTop from '@/components/common/ScrollToTop.vue';
-import FloatingPhone from '@/components/common/FloatingPhone.vue';
-import { ref } from 'vue';
-import MobileNav from './components/common/MobileNav.vue';
-const scrollVisible = ref(false)
+import { RouterView } from "vue-router";
+import Footer from "@/components/layout/Footer.vue";
+import Header from "@/components/layout/Header.vue";
+import ScrollToTop from "@/components/common/ScrollToTop.vue";
+import FloatingPhone from "@/components/common/FloatingPhone.vue";
+import { ref } from "vue";
+import MobileNav from "./components/common/MobileNav.vue";
+const scrollVisible = ref(false);
 const isMobileMenuOpen = ref(false);
-
 </script>
 
 <template>
-  <Header v-model:isMobileMenuOpen="isMobileMenuOpen"/>
+  <Header v-model:isMobileMenuOpen="isMobileMenuOpen" />
 
-  <RouterView />
+  <main class="pb-[calc(80px+env(safe-area-inset-bottom))] lg:pb-0">
+    <RouterView />
+  </main>
 
   <Footer />
   <MobileNav :is-mobile-menu-open="isMobileMenuOpen" />
